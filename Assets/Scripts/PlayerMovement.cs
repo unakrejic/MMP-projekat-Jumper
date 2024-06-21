bd6e7bbe72d2ebfe46ec3c53f0035d63f0407f2c
ergaractamara <ergarac.t@gmail.com>
2024-06-21 22:11:49 +0200

    Added comments for PlayerMovement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // 4 privatne varijable
-    private float horizontal;
-    private float speed = 3f;
-    private float jumpingPower = 12f;
-    private bool isFacingRight = true;
+    private float horizontal; // Za horizontlano kretanje
+    private float speed = 3f; // Brzina kretanja
+    private float jumpingPower = 12f; // Snaga skoka
+    private bool isFacingRight = true; // Da li je okrenut udesno
     // referenciranje na komponente
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Rigidbody2D rb; // Rigidbody2D komponenta za fiziku
+    [SerializeField] private Transform groundCheck; // Transform komponenta za proveru tla
+    [SerializeField] private LayerMask groundLayer; // LayerMask za sloj tla
 
     void Update()
     {
-        // vraca -1 || 0 || 1 u zavisnosti od pravca kretanja
+        // Uzimanje horizontalnog inputa (-1, 0, 1)
+        // vraca -1 || 0 || 1 u zavisnosti od pravca kretanja, -1 za levo a 1 za desno
         horizontal = Input.GetAxisRaw("Horizontal");
-        // ako pritisnemo space i player je na zemlji
+        // Proverava da li je pritisnut taster za skok i da li je igrač na zemlji
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             // postavljamo vrednost Ya za Velocity u okviru Rigidbody 2D na vrednost jumpingPower
+            // Postavljanje vertikalne brzine za skok
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
         // pozivamo nasu kreiranu metodu
@@ -31,25 +33,28 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // postavljamo vrednost Xa za Velocity u okviru Rigidbody 2D
+          // Postavljanje horizontalne brzine
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
     // metoda da proveri da li je player na zemlji
     private bool IsGrounded()
     {
-        // vraca true ili false da li moze player skociti
+        // vraca true ili false da li moze player skociti, tj vraca true ako je igrac na sloju tla
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
     // metoda da nam okrene player-a
     private void Flip()
     {
+        // Proverava da li igrač treba da se okrene
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
-            // postavljamo suprotnu vrednost
+            // postavljamo suprotnu vrednost, menja smer igraca
             isFacingRight = !isFacingRight;
             // uzimamo vrednost za scale of player-a
             Vector3 localScale = transform.localScale;
             // mnozimo X komponentu Scale-a za playera sa -1
             localScale.x *= -1f;
+            // Primena nove scale-a
             transform.localScale = localScale;
         }
     }
